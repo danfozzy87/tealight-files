8d81dddfd4ee199e5c386de914f3615a492bb8a6
danfozzy87 <fozzy4u@msn.com>
2014-08-19 14:44:17 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -8,24 +8,24 @@ from tealight.robot import (move,
 
 
 
-def recursion(count):
+def recursion(count, end):
   if touch() == 'fruit':
     move()
-    recursion(count+1)
+    recursion(count+1, false)
     
   if left_side() == 'fruit':
     turn(-1)
-    recursion(0)
+    recursion(0, false)
     
   if right_side() == 'fruit':
     turn(1)
-    recursion(0)
+    recursion(0, false)
   
-  
-  turn(2)
-  for i in range(0,count):
-    move()
-  
-  recursion(0)
+  if end == false:
+    turn(2)
+    for i in range(0,count):
+      move()
+    recursion(0, true)
+    
     
 recursion(0)
