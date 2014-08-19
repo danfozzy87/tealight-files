c8d623058787cccd7396e79dcf35a5990750878d
danfozzy87 <fozzy4u@msn.com>
2014-08-19 14:20:32 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -7,3 +7,15 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+
+def recursion():
+  if touch() = 'fruit':
+    move()
+  if left_side() == 'fruit':
+    turn(-1)
+    recursion()
+  if right_side() == 'fruit':
+    turn(1)
+    recursion()
+    
+    
