925a8d532415f47b160cdd662f444fbf4dc503ce
danfozzy87 <fozzy4u@msn.com>
2014-08-19 14:48:13 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -17,15 +17,17 @@ def recursion(count, end):
     turn(-1)
     recursion(0, 'false')
     
-  if right_side() == 'fruit':
-    turn(1)
-    recursion(0, 'false')
+#  if right_side() == 'fruit':
+#    turn(1)
+#    recursion(0, 'false')
   
   if end == 'false':
     turn(2)
     for i in range(0,count):
       move()
     recursion(0, 'true')
-    
+  else:
+    turn(-1)
+    recursion(0, 'false')
     
 recursion(0, 'false')
